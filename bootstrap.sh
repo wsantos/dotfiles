3632af9b5ca5633bc956e1e686f3e28e34aeb2f4
Waldecir Santos <waldecir@gmail.com>
2015-06-25 15:10:02 -0300

    fixed bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-ln -s ~/.dotfiles/gitconfig 	~/.gitconfig
-ln -s ~/.dotfiles/gitignore 	~/.gitignore
-ln -s ~/.dotfiles/gvimrc 	~/.gvimrc
-ln -s ~/.dotfiles/vim		~/.vim
-ln -s ~/.dotfiles/vimrc		~/.vimrc
+ln -s ~/dotfiles/gitconfig 	~/.gitconfig
+ln -s ~/dotfiles/gitignore 	~/.gitignore
+ln -s ~/dotfiles/gvimrc 	~/.gvimrc
+ln -s ~/dotfiles/vim		~/.vim
+ln -s ~/dotfiles/vimrc		~/.vimrc
 
